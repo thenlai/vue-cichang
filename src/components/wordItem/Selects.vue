3349f424f804494bf1350b02fd485b4829bfac04
thenlai <tl_huang@outlook.com>
2016-10-20 18:14:51 +0800

    add: wordItem

---
+++ src/components/wordItem/Selects.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li>
+      <input type="text" >
+    </li>
+    <li>
+      <input type="text" >
+    </li>
+    <li>
+      <input type="text" >
+    </li>
+    <li>
+      <input type="text" >
+  </li>
+  <ul>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
