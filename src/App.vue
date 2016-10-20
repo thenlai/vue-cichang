64a29883b47a4849f0983bfd3926fcf6406903ae
thenlai <tl_huang@outlook.com>
2016-10-21 00:00:18 +0800

    update: Selection

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <selection :index="option.index" :selections="option.selections"></selection>
+    <header>
+      <button class="action quit">quit</button>
+      <span class="progress-bar">
+
+      </span>
+      <span class="statistics"></span>
+    </header>
+    <selection :hint="option.hint" :index="option.index" :selections="option.selections"></selection>
   </div>
 </template>
 
@@ -15,8 +22,9 @@ export default {
   data: function () {
     return {
       option: {
+        hint: 'wind',
         index: 1,
-        selections: ['abka', 'edun', 'na', 'ula']
+        selections: ['そら', 'かぜ', 'あめ', 'かわ']
       }
     }
   }
@@ -31,5 +39,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 10px;
+  max-width: 640px;
+  margin: 0 auto;
+}
+* {
+  -webkit-touch-callout:none;
+  -webkit-user-select:none;
+  -moz-user-select:none;
+  -ms-user-select:none;
+  user-select:none;
+}
+input, textarea, .selectable {
+    -webkit-touch-callout:default;
+    -webkit-user-select:text;
+    -moz-user-select:text;
+    -ms-user-select:text;
+    user-select:text;
 }
 </style>
--- src/components/selection/Selection.vue
+++ src/components/selection/Selection.vue
@@ -1,16 +1,23 @@
 <template>
-  <ul :class='["selection-list", {error: isError}]'>
-    <li
-    v-for='(item, i) in selections'
-    @click.stop='submit(i)'
-    :class='["selection", {"selected": selectedIndex===i}]'
-    >{{item}}</li>
-  </ul>
+  <section>
+    <h1 class="hint">{{hint}}</h1>
+    <ul :class='["selection-list", {error: isError}]'>
+      <li
+      v-for='(item, i) in selections'
+      @click.stop='submit(i)'
+      :class='["selection", {"selected": selectedIndex===i}, {"answer": index===i}]'
+      >{{item}}</li>
+    </ul>
+    <div v-if="isError" class="action-group">
+      <button class="action view">view</button>
+      <button class="action next">next</button>
+    </div>
+  </section>
 </template>
 
 <script>
   export default {
-    props: ['selections', 'index'],
+    props: ['hint', 'selections', 'index'],
     methods: {
       submit: function (i) {
         if (this.selectedIndex !== null) {
@@ -35,6 +42,11 @@
 </script>
 
 <style scoped>
+  .hint {
+    font-size: 2rem;
+    text-align: center;
+    margin: 2rem 0.8rem 3rem 0.8rem;
+  }
   .selection-list {
     padding: 0;
     margin: 0;
@@ -42,9 +54,9 @@
   .selection {
     cursor: pointer;
     list-style: none;
-    padding: 0.5rem 1rem;
+    padding: 0.6rem 1rem;
     font-size: 1.4rem;
-    background-color: rgba(249,249,249,.8);
+    background-color: rgba(240,240,240,.8);
     margin-top: 0.8rem;
     border-radius: 0.2rem;
   }
@@ -56,7 +68,51 @@
     color: red;
     background-color: #ffdede;
   }
+  .error .answer {
+    background-color: #dcffdc;
+    color: green;
+  }
+  .answer.selected {
+    background-color: #dcffdc;
+    color: green;
+  }
   .selected {
-    text-decoration: line-through;
+    animation: 0.6s selected alternate;
+  }
+  @keyframes selected {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(0.9);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  .action-group {
+    display: flex;
+    justify-content: space-between;
+  }
+  .action-group .action {
+    flex: 1;
+    padding: 0.6rem;
+    background: none;
+    border: none;
+    outline: none;
+    font-size: 1.2rem;
+    margin: 2rem 0 0 0;
+    border-radius: 0.2rem;
+  }
+  .action-group .action.view {
+    background: #333;
+    color: #fff;
+    margin-right: 0.5rem;
+  }
+  .action-group .action.next {
+    background: #eee;
+    color: #111;
+    margin-left: 0.5rem;
   }
 </style>
